bfdd90a32e4c76aa4b024e34ae87449fd3c301ce
xxq <1062061714@qq.com>
2019-10-21 20:52:38 +0800

    helloWORD java hhhh

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -2,5 +2,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("hello kafka");
+        System.out.println("hello text");
     }
 }
